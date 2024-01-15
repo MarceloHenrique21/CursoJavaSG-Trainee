4a29ded866588db124352286d322e6d740b5be17
feitan02 <mh192373@gmail.com>
2024-01-15 20:22:11 -0300

    criando o projeto com spring

---
+++ semanaDois/src/test/java/Produto/ProdutoAPI.java
@@ -0,0 +1,15 @@
+package Produto;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/produto")
+public class ProdutoAPI {
+
+    @GetMapping("/teste")
+    public ResponseEntity<String> teste(){
+        return ResponseEntity.ok("teste");
+    }
+}
---
+++ semanaDois/src/test/java/com/semanaDois/semanaDois/SemanaDoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semanaDois.semanaDois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanaDoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
