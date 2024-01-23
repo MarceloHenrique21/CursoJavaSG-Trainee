408479fba83a1dc464bf3191735f0fefc46b9c9c
feitan02 <mh192373@gmail.com>
2024-01-22 21:28:24 -0300

    as

--- Cinema/src/main/java/com/Cinema/Cinema/Filme/FilmeService.java
+++ Cinema/src/main/java/com/Cinema/Cinema/Filme/FilmeService.java
@@ -5,15 +5,14 @@ import com.Cinema.Cinema.DTO.GerarSessoesAssentos;
 import com.Cinema.Cinema.Sessao.Sessao;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class FilmeService {
